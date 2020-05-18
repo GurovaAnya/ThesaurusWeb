2440bf7ef62ce58ed9cc0bf0851a83665114f4dc
Анна Гурова <aagurova_2@edu.hse.ru>
2020-05-18 20:21:28 +0500

    just started

---
+++ DefinitionExtractionWeb/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using DefinitionExtractionWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DefinitionExtractionWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        DEModel db = new DEModel();
+        public ActionResult Index()
+        {
+            IEnumerable<Descriptor> desc = db.Descriptors;
+            ViewBag.Descriptors = desc;
+            return View();
+        }
+
+        public ActionResult DefinitionView(int id)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ DefinitionExtractionWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DefinitionExtractionWeb.Startup))]
+namespace DefinitionExtractionWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
