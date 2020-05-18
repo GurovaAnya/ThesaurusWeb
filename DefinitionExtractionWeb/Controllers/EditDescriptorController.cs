f97f15efdc170526dd340c239a0d45db7da1375c
Анна Гурова <aagurova_2@edu.hse.ru>
2020-05-18 23:05:57 +0500

    Edit descriptor

--- DefinitionExtractionWeb/Controllers/EditDescriptorController.cs
+++ DefinitionExtractionWeb/Controllers/EditDescriptorController.cs
@@ -1,35 +1,32 @@
-﻿using DefinitionExtractionWeb.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace DefinitionExtractionWeb.Controllers
 {
-    public class DefinitionsController : Controller
+    public class EditDescriptorController : Controller
     {
-        DEModel db = new DEModel();
-        // GET: Definitions
+        // GET: EditDescriptor
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: Definitions/Details/5
+        // GET: EditDescriptor/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Definitions/Create
+        // GET: EditDescriptor/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Definitions/Create
+        // POST: EditDescriptor/Create
         [HttpPost]
         public ActionResult Create(FormCollection collection)
         {
@@ -45,37 +42,13 @@ namespace DefinitionExtractionWeb.Controllers
             }
         }
 
-        // GET: Definitions/Edit/5
+        // GET: EditDescriptor/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        [HttpGet]
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return HttpNotFound();
-            }
-            Descriptor desc = db.Descriptors.Find(id);
-
-            if (desc == null)
-            {
-                return HttpNotFound();
-            }
-            return View(desc);
-        }
-
-        [HttpPost]
-        public ActionResult Edit(Descriptor descriptor)
-        {
-            db.Entry(descriptor).State = EntityState.Modified;
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        // POST: Definitions/Edit/5
+        // POST: EditDescriptor/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
@@ -91,13 +64,13 @@ namespace DefinitionExtractionWeb.Controllers
             }
         }
 
-        // GET: Definitions/Delete/5
+        // GET: EditDescriptor/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: Definitions/Delete/5
+        // POST: EditDescriptor/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
